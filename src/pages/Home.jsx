e94d533594826b4d976d6920d0cac8a446c48bb6
bilalabdelkadir <bilalbinabdelkadir>
2023-03-15 17:00:36 +0300

    i added home page that includes navbar hero cards and footer components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div className="main-bg h-[200vh]">
+      <NavBar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
