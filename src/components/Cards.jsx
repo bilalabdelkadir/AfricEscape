8ea77bdb1e1d5ff7c7ed2caef01041bfe355750c
bilalabdelkadir <bilalbinabdelkadir>
2023-03-16 17:31:55 +0300

    i fixed the margin issue on small devices

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const Cards = () => {
   return (
-    <div className="flex justify-center place-items-stretch flex-wrap max-w-[1200px] gap-4 mx-auto">
+    <div className="flex justify-center place-items-stretch flex-wrap max-w-[1200px] gap-4 mx-2 md:mx-auto">
       <Card />
       <Card />
       <Card />
