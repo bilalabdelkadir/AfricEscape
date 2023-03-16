8db02b2a3fbdd91833f82362574f2315f4ec8b68
bilalabdelkadir <bilalbinabdelkadir>
2023-03-16 17:23:50 +0300

    i added search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { FiSearch } from "react-icons/fi";
-import PIC from "../assets/nature_1.jpg";
 const SearchBar = () => {
   return (
-    <div className="my-10 border-2 border-primary w-[90%] md:w-[50%] mx-auto p-1 md:p-1 rounded-full ">
+    <div className="my-10 border border-gray-300 w-[90%] md:w-[50%] mx-auto p-1 md:p-1 rounded-full">
       <div className="flex justify-between pl-5 items-center ">
         <form className="w-[90%]">
           <input
-            className="w-full h-full text-2xl outline-none"
+            className="w-full h-full text-lg outline-none"
             type="search"
+            placeholder="Search Your destination here"
           />
         </form>
         <div className="bg-primary rounded-full p-2 sm:p-1 cursor-pointer">
