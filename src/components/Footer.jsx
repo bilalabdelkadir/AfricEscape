b2fa6a05cd725470a04b41ec7d5c8bfda2af0bf1
bilalabdelkadir <bilalbinabdelkadir>
2023-03-17 18:23:15 +0300

    i fixed some styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -42,9 +42,9 @@ const Footer = () => {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <span className="block flex items-center justify-center text-sm text-gray-500 sm:text-center ">
+        <span className="flex items-center justify-center text-sm text-gray-500 sm:text-center ">
           ©
-          <a href="https://flowbite.com/" class="hover:underline">
+          <a href="https://flowbite.com/" className="hover:underline">
             AfricEscape™
           </a>
           . All Rights Reserved.
