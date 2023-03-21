458f0bd22287ff8a66ba4d32fadcf2cbe3c4faaa
bilalabdelkadir <bilalbinabdelkadir>
2023-03-21 14:28:47 +0300

    updates the card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,29 +1,25 @@
 import React, { useState } from "react";
-import Slider from "react-slick";
-import Nature_1 from "../assets/nature_1.jpg";
-import Nature_2 from "../assets/nature_2.jpg";
 import Nature_3 from "../assets/nature_3.jpg";
-import Nature_4 from "../assets/nature_4.jpg";
-import { FiHeart, FiStar } from "react-icons/fi";
+import { FiStar } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ tour }) => {
   return (
     <Link
-      to="/"
-      className="sm:w-[47%] xs:w-[90%] md:w-[32%] lg:w-[24%] shadow-lg overflow-hidden rounded-lg"
+      to={`/tours/${tour.slug}/${tour.id}`}
+      className="sm:w-[47%] relative xs:w-[90%] md:w-[30%] shadow-lg lg:w-[27%] lg:m-4 new-shadow  overflow-hidden rounded-lg md:h-[27rem] h-[30rem]"
     >
-      <div>
+      <div className="h-[45%]">
         <img
           className="w-full h-full object-cover"
-          src={Nature_2}
+          src={`http://127.0.0.1:3000/${tour.imageCover}` || Nature_3}
           alt="Mountain"
         />
       </div>
 
       <div className="p-4">
         <div className="flex justify-start w-full h-full items-center border-b-2">
-          <div className="w-[80%] my-2">
+          {/* <div className="w-[80%] my-2">
             <div className="flex justify-start">
               <p>Region: </p>
               <p>Afar</p>
@@ -32,23 +28,22 @@ const Card = () => {
               <p>City: </p>
               <p>Abala</p>
             </div>
-          </div>
+          </div> */}
           <div className="flex justify-start items-center my-2">
             <FiStar className="mx-1" />
-            <p>4.7</p>
+            <p>{tour.ratingsAverage}</p>
           </div>
         </div>
-        <h1 className="font-semibold text-xl">Abala Volcano</h1>
+        <h1 className="font-semibold mt-4 text-xl">{tour.name}</h1>
         <div>
-          <p className="font-[400] text-gray-400 text-sm ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            numquam laboriosam ex non commodi rerum, praesentium.
+          <p className="font-[400] text-gray-600 text-sm pb-6">
+            {tour.summary}
           </p>
         </div>
       </div>
-      <div className="w-full">
-        <p className="mt-2 px-4 h-[3rem] flex-nav bg-gray-100 backdrop-blur-lg">
-          Estimated Price $250 - $350
+      <div style={{ position: "absolute", bottom: 0, width: "100%" }}>
+        <p className="mt-2 px-4 h-[3rem] flex-nav bg-green-100 backdrop-blur-lg">
+          Estimated Price ${tour.price}
         </p>
       </div>
     </Link>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { TourContext } from "../context/TourContext";
 import Card from "./Card";
+import { fetchTours } from "../utils/api";
+import Spinner from "./Spinner";
 
 const Cards = () => {
-  return (
-    <div className="flex justify-center place-items-stretch flex-wrap max-w-[1200px] gap-4 mx-2 md:mx-auto">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  );
+  const { tours, setTours } = useContext(TourContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getTours() {
+      try {
+        const toursData = await fetchTours();
+        setTours(toursData);
+        setIsLoading(false);
+        console.log(toursData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getTours();
+  }, []);
+
+  if (isLoading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="flex justify-center place-items-stretch flex-wrap max-w-[1200px] gap-4 mx-2 md:mx-auto">
+        {tours.map((tour) => (
+          <Card key={tour.id} tour={tour} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Cards;
