a7aee54e61748ccf65c464047874849a2a363af2
bilalabdelkadir <bilalbinabdelkadir>
2023-03-24 10:03:05 +0300

    added new fetch functions for fetching the user and tours

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchTours = async () => {
   try {
-    const response = await axios.get("http://127.0.0.1:3000/api/v1/tours");
+    const response = await axios.get(
+      "https://africescape-api.onrender.com/api/v1/tours"
+    );
     return response.data.data.data;
   } catch (error) {
     return error.message;
@@ -13,10 +15,27 @@ export const fetchTours = async () => {
 export const fetchTour = async (id) => {
   try {
     const response = await axios.get(
-      `http://127.0.0.1:3000/api/v1/tours/${id}`
+      `https://africescape-api.onrender.com/api/v1/tours/${id}`
     );
     return response.data.data.data;
   } catch (error) {
     return error.message;
+  }
+};
+
+export const fetchUser = async (token) => {
+  try {
+    const response = await axios.get(
+      `https://africescape-api.onrender.com/api/v1/users/me`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    // console.log(response.data.data);
+    return response.data.data;
+  } catch (error) {
+    console.log(error.message);
   }
 };
