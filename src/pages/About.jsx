766593431b15ad3e363ca9c8257cadb859b8bf13
bilalabdelkadir <bilalbinabdelkadir>
2023-03-28 00:40:52 +0300

    i updated the about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,125 +4,57 @@ import Footer from "../components/Footer";
 
 const About = () => {
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-100">
       <NavBar />
-      <div className="relative top-20 max-w-[1200px] md:mx-auto bg-white">
-        <h1>About Us</h1>
-        <p className="p-4 md:p-20">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
-          excepturi asperiores fuga, labore inventore harum distinctio! Optio
-          quo expedita numquam culpa nisi amet, pariatur ipsum cum animi ea a
-          alias natus, debitis est deserunt dolor blanditiis fugit obcaecati hic
-          accusantium quae tempore! Quidem, maiores ad, dolores modi facilis
-          rerum ipsa voluptate error fuga quia iusto delectus vel? Tenetur
-          laboriosam quaerat blanditiis totam saepe. Consequatur corrupti
-          eveniet sint. Aperiam nulla nostrum repellendus assumenda, ducimus
-          nobis eveniet id hic exercitationem quos dicta excepturi officiis
-          nesciunt facilis nam quod ad eum soluta pariatur dolorem labore! Iure
-          provident earum voluptatum deleniti. In, voluptatum a natus ipsum
-          maiores pariatur, id eligendi assumenda atque quaerat iure aliquam
-          odit reiciendis velit placeat molestiae nihil dolorem, nesciunt magni
-          explicabo reprehenderit corrupti nulla repellat! Voluptates
-          reprehenderit mollitia blanditiis laboriosam. Quas, nam. Dolores
-          tenetur accusantium soluta distinctio minus, in, reprehenderit earum
-          consequuntur illo ea neque velit minima delectus dolore doloremque
-          aliquam. Qui, dolore delectus maxime maiores consequuntur voluptas
-          culpa est impedit libero officiis ipsa error itaque adipisci sed quo
-          vero consequatur recusandae! Adipisci officia minus consequuntur omnis
-          quam, accusantium nemo aliquam facilis rem pariatur quis unde officiis
-          sint explicabo earum ad, eum neque! Earum, impedit accusantium
-          possimus inventore saepe labore nobis necessitatibus? Ducimus, velit
-          doloribus quaerat enim placeat dolorum nam labore incidunt expedita
-          voluptatem consequuntur blanditiis ea ipsum et, iste fugit illum
-          omnis? Nisi iure molestias voluptas, ea ex vitae assumenda. A,
-          molestias dignissimos reiciendis eum tenetur ex pariatur deserunt quod
-          quidem doloremque ipsa nihil cupiditate iusto rem impedit obcaecati,
-          modi natus. Reiciendis, ipsam. Totam animi sequi pariatur aut magnam
-          repellendus possimus iusto molestias ratione hic numquam ipsa
-          praesentium rem ullam exercitationem nobis libero fugit, dicta
-          voluptatibus velit voluptates corrupti? Optio, quos porro facilis
-          perferendis doloribus recusandae numquam sequi blanditiis at, quia
-          fugiat ipsum consectetur a qui? Possimus esse corporis enim quo?
-          Mollitia, optio animi? Dolorem excepturi reiciendis eveniet eaque
-          corporis, est, voluptas distinctio ex doloremque soluta magnam facilis
-          at? Ipsa, sunt asperiores rerum laborum officia illo voluptatibus
-          accusantium repellat quos unde itaque! Voluptatum culpa provident
-          cupiditate impedit ducimus, qui cumque veniam commodi laudantium sequi
-          voluptatem debitis reiciendis quia optio ex doloribus expedita facilis
-          minima quidem repudiandae incidunt! Expedita consequatur facilis
-          debitis, tempore facere repellendus porro atque? Reprehenderit
-          deserunt et distinctio tenetur doloremque illum sunt odio. Quia
-          necessitatibus corrupti quos nesciunt repellat a voluptate,
-          reprehenderit magni commodi, sequi tenetur aut? Facere quos quo eaque
-          neque eum optio molestiae est amet dolorum porro, expedita repellat
-          assumenda reprehenderit laboriosam maxime eius accusamus soluta
-          minima, pariatur officiis quibusdam? Neque odio nulla, maiores facilis
-          laudantium magni porro sequi eligendi aperiam sint doloremque sapiente
-          dolores beatae nam inventore officiis qui repudiandae possimus at!
-          Eligendi atque rem inventore temporibus facilis mollitia. Tenetur modi
-          obcaecati quasi quam, atque, natus labore reiciendis eius deserunt
-          nisi consequatur itaque? Consequatur, laudantium, nam deserunt
-          expedita minima quisquam libero est voluptatum aliquid hic amet. Neque
-          libero possimus fuga est, odit iusto. Ratione autem qui error
-          praesentium dolorum fugit soluta corrupti ab laudantium consectetur,
-          temporibus neque aliquid cupiditate omnis ex tempora? Eveniet ratione
-          voluptatibus corporis, iusto asperiores reprehenderit! Vitae ratione
-          reprehenderit ea quisquam, rem dolorum veritatis cupiditate ex sint
-          iure fuga debitis beatae soluta esse voluptatibus porro repudiandae,
-          ad aut tempora corporis odio? Impedit iure, iste modi quam nobis
-          maxime tempore est optio rem eligendi esse neque eaque labore sunt
-          sequi, enim sapiente! Quia, laboriosam consectetur labore distinctio
-          sunt saepe provident, non vero numquam eaque quidem natus libero ipsa
-          consequuntur id voluptates, delectus fugit sint. Corrupti, ullam animi
-          deserunt voluptate soluta magni nulla minima, et temporibus eligendi
-          dolore. Quod voluptatem expedita, voluptate commodi nisi autem iure
-          fugit magnam illo hic magni eius dicta voluptatum dignissimos velit
-          veritatis ducimus consequuntur sed totam sit atque nostrum quae nobis
-          nam? Facilis hic incidunt nam rem ut, animi quod ullam assumenda, quis
-          obcaecati, distinctio fuga provident minus explicabo sunt soluta vero
-          ipsa rerum praesentium expedita maxime sed. Porro quibusdam
-          voluptatibus minima ratione soluta? Temporibus, inventore quod?
-          Quaerat odit praesentium unde fugit velit iure nisi autem non
-          dignissimos doloribus voluptatum perspiciatis quae sequi voluptates
-          odio porro rerum quas qui mollitia, fugiat sit doloremque. Nam aliquam
-          qui atque illum, dolor nemo? Eaque modi, dolore ea doloribus sed vel
-          maiores sit. Eaque voluptates dolore possimus debitis sed velit
-          repudiandae quis similique non, quas laboriosam placeat expedita culpa
-          modi quasi sapiente alias maiores repellendus libero eos labore aut
-          ipsam? Non saepe iusto nisi repellat sed dolore illo quae. Consequatur
-          ad excepturi ducimus adipisci nam repudiandae nobis placeat. Voluptas
-          illum placeat sunt obcaecati aut commodi accusantium ut aspernatur
-          dolorum incidunt rem, ratione, in est ipsum, veniam vel nostrum error
-          illo quibusdam suscipit. Vitae possimus odio amet recusandae,
-          perferendis fuga fugit earum quas expedita esse distinctio assumenda
-          eius quos harum autem voluptas magni ea hic atque quod quia? Fuga
-          officiis cupiditate qui amet voluptas, modi exercitationem minima
-          harum similique aspernatur recusandae ipsam labore reprehenderit eos
-          dicta rerum itaque, animi adipisci vel, expedita aliquid esse
-          accusantium est? Eveniet excepturi, totam architecto quo asperiores
-          blanditiis delectus pariatur dignissimos, voluptatem, recusandae
-          nesciunt temporibus soluta. Eligendi quia ipsam debitis? Neque, libero
-          ipsa molestias quam provident nulla reprehenderit quaerat! Error vitae
-          eius, necessitatibus odio ducimus laudantium temporibus asperiores
-          amet architecto, laborum quod nihil, sunt quos fugiat id distinctio
-          consequuntur libero? Autem modi quis tenetur voluptatem ullam velit
-          sunt, cum mollitia veritatis? Harum accusantium consequuntur
-          laboriosam in! Illum accusamus ipsa, voluptates alias vero libero
-          harum. Soluta, recusandae iste sapiente consequatur esse quibusdam
-          culpa quam blanditiis doloremque quos temporibus consectetur,
-          similique corporis rem, fuga molestiae deserunt at amet. Explicabo
-          laudantium amet impedit tenetur? Quia temporibus maiores repellendus
-          ad iure. Perspiciatis, nisi. Dolore repudiandae aliquid iusto rerum
-          nam, fuga, sint nulla dicta reprehenderit, architecto ea nobis
-          distinctio ipsa ducimus! Ullam soluta accusamus reiciendis quibusdam,
-          inventore mollitia rem doloremque explicabo. Veniam provident
-          quibusdam vero debitis ipsa possimus aliquid officiis unde alias,
-          nostrum perferendis totam tenetur aperiam ut, ipsam molestiae tempora
-          amet saepe, eligendi expedita at ullam minima? Earum vitae voluptas,
-          dolore suscipit adipisci consequatur magnam aspernatur doloremque
-          laborum provident natus amet dolorum expedita repellat sequi! Amet
-          sint asperiores repellendus vero ipsam nulla!{" "}
-        </p>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+        <h1 className="text-4xl font-bold mb-8">About Us</h1>
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 md:pr-8 mb-8 md:mb-0">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzQx3_jDzkzajOoZWxJ1gsqyzLWzW39oUAZnP-d3HX&s"
+              alt="About Us"
+              className="w-full h-auto rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="md:w-1/2">
+            <p className="text-lg leading-relaxed mb-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tincidunt, erat in malesuada aliquam, est erat faucibus purus,
+              eget viverra nulla sem vitae neque. Quisque id sodales libero. In
+              nec enim nisi. Nullam quis augue ligula. Nulla facilisi.
+              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+              posuere cubilia curae; Nam blandit, velit eget commodo dignissim,
+              sem nibh laoreet ipsum, vel tincidunt ligula enim vitae purus.
+              Etiam sed nibh feugiat, condimentum ipsum nec, faucibus odio.
+              Donec euismod vestibulum massa, ac volutpat libero ornare non.
+              Nullam eget velit ante. Praesent placerat sapien mauris, vitae
+              sodales tellus venenatis ac. Nullam vel justo vitae massa
+              ullamcorper euismod. Nulla facilisi. Sed ut erat ligula. Nam
+              tincidunt vestibulum libero, non fringilla enim faucibus sed. Sed
+              eu scelerisque dui. Sed eu arcu at nibh hendrerit viverra. Vivamus
+              facilisis volutpat odio, ac tempor velit efficitur et. Sed
+              vestibulum, felis quis pharetra tincidunt, nunc libero bibendum
+              massa, in faucibus dolor lorem vitae metus. Sed nec nulla
+              dignissim, bibendum velit eget, ultrices elit. Sed euismod
+              tincidunt nisl, ac vestibulum elit vehicula eu. Sed euismod
+              tincidunt nisl, ac vestibulum elit vehicula eu.
+            </p>
+            <p className="text-lg leading-relaxed mb-8">
+              Sed euismod tincidunt nisl, ac vestibulum elit vehicula eu. Sed
+              euismod tincidunt nisl, ac vestibulum elit vehicula eu. Sed
+              vestibulum, felis quis pharetra tincidunt, nunc libero bibendum
+              massa, in faucibus dolor lorem vitae metus. Sed nec nulla
+              dignissim, bibendum velit eget, ultrices elit. Sed euismod
+              tincidunt nisl, ac vestibulum elit vehicula eu. Sed euismod
+              tincidunt nisl, ac vestibulum elit vehicula eu. Sed vestibulum,
+              felis quis pharetra tincidunt, nunc libero bibendum massa, in
+              faucibus dolor lorem vitae metus. Sed nec nulla dignissim,
+              bibendum velit eget, ultrices elit. Sed euismod tincidunt nisl, ac
+              vestibulum elit vehicula eu. Sed euismod tincidunt nisl, ac
+              vestibulum elit vehicula eu.
+            </p>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
