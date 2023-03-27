59e595945116897e6ad1f76dd6dd03b88b73b2cf
bilalabdelkadir <bilalbinabdelkadir>
2023-03-27 15:54:41 +0300

    added api util

--- src/utils/api.js
+++ src/utils/api.js
@@ -39,3 +39,52 @@ export const fetchUser = async (token) => {
     console.log(error.message);
   }
 };
+
+export const fetchMyBooking = async (token) => {
+  try {
+    const response = await axios.get(
+      `https://africescape-api.onrender.com/api/v1/bookings/myBooking`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    // console.log(response.data.data);
+    return response.data.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const cancelBooking = async (token, bookingId) => {
+  try {
+    const response = await axios.delete(
+      `https://africescape-api.onrender.com/api/v1/bookings/${bookingId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return "success";
+  } catch (error) {
+    return error.message;
+  }
+};
+
+export const fetchUserReviews = async (token) => {
+  try {
+    const response = await axios.get(
+      `https://africescape-api.onrender.com/api/v1/users/reviews`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
