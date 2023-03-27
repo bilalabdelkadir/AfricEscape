55b13247e39bda9cd0cd00f81908ae6b19745748
bilalabdelkadir <bilalbinabdelkadir>
2023-03-27 17:14:43 +0300

    updated the rating system

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -5,25 +5,17 @@ function Rating({ rating }) {
   const emptyStars = 5 - filledStars;
 
   return (
-    <div className="flex">
+    <div class="flex items-center mb-5">
       {[...Array(filledStars)].map((_, i) => (
         <svg
-          key={i}
-          className="w-4 h-4 fill-current text-yellow-400"
-          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          class="w-5 h-5 text-yellow-400"
+          fill="currentColor"
           viewBox="0 0 20 20"
-        >
-          <path d="M10 0l2.5 7.5H20l-6.25 4.583 2.5 7.917L10 15l-6.25 4.583 2.5-7.917L0 7.5h7.5z" />
-        </svg>
-      ))}
-      {[...Array(emptyStars)].map((_, i) => (
-        <svg
-          key={i}
-          className="w-5 h-5 fill-current text-gray-300"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
         >
-          <path d="M10 0l2.5 7.5H20l-6.25 4.583 2.5 7.917L10 15l-6.25 4.583 2.5-7.917L0 7.5h7.5z" />
+          <title>First star</title>
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
         </svg>
       ))}
     </div>
